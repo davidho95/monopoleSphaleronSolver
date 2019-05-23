8ddb700279cbf12f7a2e5c23d3eeff6086432380
David Ho <dlh16@dyn195-199.dyn.ic.ac.uk>
2019-05-23 13:01:11 +0100

    altered boundary conditions so 't Hooft lines are no longer required

--- src/GeorgiGlashowSu2TheoryUnitary.hpp
+++ src/GeorgiGlashowSu2TheoryUnitary.hpp
@@ -14,6 +14,8 @@ namespace monsta {
     std::complex<double> getLocalGradient(LATfield2::Field< std::complex<double> > &field, LATfield2::Site &site, int matIdx, int rowIdx, int colIdx) const;
     monsta::Matrix getLocalGradient(LATfield2::Field< std::complex<double> > &field, LATfield2::Site &site, int matIdx) const;
     double getVev() const { return vev_; }
+    double getGaugeCoupling() const { return gaugeCoupling_; }
+    double getSelfCoupling() const { return selfCoupling_; }
     double getMagneticField(LATfield2::Field< std::complex<double> > &field, LATfield2::Site &site, int dir) const;
     void postProcess(LATfield2::Field< std::complex<double> > &field, LATfield2::Site &site, int matIdx) const;
     void applyBoundaryConditions(LATfield2::Field< std::complex<double> > &field) const;
@@ -23,6 +25,8 @@ namespace monsta {
     void applyDirichletBoundaryConditions(LATfield2::Field< std::complex<double> > &field) const;
     void applyCPeriodicBoundaryConditions(LATfield2::Field< std::complex<double> > &field, int dir) const;
     void applyTwistedBoundaryConditions(LATfield2::Field< std::complex<double> > &field, int dir) const;
+    void applyTwistedBoundaryConditions2(LATfield2::Field< std::complex<double> > &field, int dir) const;
+    void applyTwistedBoundaryConditions3(LATfield2::Field< std::complex<double> > &field, int dir) const;
     monsta::Matrix getPlaquette(LATfield2::Field< std::complex<double> > &field, LATfield2::Site &site, int dir1, int dir2) const;
     monsta::Matrix getStaple(LATfield2::Field< std::complex<double> > &field, LATfield2::Site &site, int dir1, int dir2, bool isUp) const;
     double getTHooftOperator(LATfield2::Field< std::complex<double> > &field, LATfield2::Site &site, int dir, std::complex<double> fluxQuanta=1) const;
@@ -67,13 +71,13 @@ namespace monsta {
     E += 2.0/pow(gaugeCoupling_,2)*(2 - real(trace(getPlaquette(field, site, 2, 0))));
     E += 2.0/pow(gaugeCoupling_,2)*(2 - real(trace(getPlaquette(field, site, 0, 1))));
 
-    for (int ii = 0; ii < 3; ii++)
-    {
-      if (tHooftLineCheck(field, site, ii))
-      {
-        E += getTHooftOperator(field, site, ii, fluxQuanta_);
-      }
-    }
+    // for (int ii = 0; ii < 3; ii++)
+    // {
+    //   if (tHooftLineCheck(field, site, ii))
+    //   {
+    //     E += getTHooftOperator(field, site, ii, fluxQuanta_);
+    //   }
+    // }
 
     // Covariant Derivative
     for (int ii = 0; ii < 3; ii++)
@@ -105,7 +109,7 @@ namespace monsta {
       plaquetteDerivMat = plaquetteDerivMat + getStaple(field, site, matIdx, dir2, false);
 
       grad = grad - 2.0/pow(gaugeCoupling_,2)*plaquetteDerivMat;
-      grad = grad - 2.0/pow(gaugeCoupling_,2)*getTHooftDeriv(field, site, matIdx, fluxQuanta_);
+      // grad = grad - 2.0/pow(gaugeCoupling_,2)*getTHooftDeriv(field, site, matIdx, fluxQuanta_);
 
       // Derivative of kinetic term
       LATfield2::Site tempSite(site);
@@ -121,9 +125,6 @@ namespace monsta {
 
       // grad = grad - 0.5*trace(grad*conjugateTranspose(Matrix(field, site, matIdx)))*Matrix(field, site, matIdx);
 
-      // makeTraceless(grad);
-      // grad(0,0) = imag(grad(0,0));
-      // grad(1,1) = imag(grad(1,1));
     } else {
       // if (abs(field(site, 3, 0, 0)) < 1e-15) { return grad; }
       for (int dir = 0; dir < 3; dir++)
@@ -221,26 +222,31 @@ namespace monsta {
 
   void GeorgiGlashowSu2Theory::applyBoundaryConditions(LATfield2::Field< std::complex<double> > &field) const
   {
-    if (boundaryConditions_[0] == 0 && boundaryConditions_[1] == 0 && boundaryConditions_[2] == 0)
-    {
-      applyDirichletBoundaryConditions(field);
-      return;
-    }
+    // if (boundaryConditions_[0] == 0 && boundaryConditions_[1] == 0 && boundaryConditions_[2] == 0)
+    // {
+    //   applyDirichletBoundaryConditions(field);
+    //   return;
+    // }
     field.updateHalo();
-    // if (boundaryConditions_[0] == -1 && boundaryConditions_[1] == -1 && boundaryConditions_[2] == -1)
+    // if (boundaryConditions_[0] == 2 && boundaryConditions_[1] == 0 && boundaryConditions_[2] == 0)
     // {
     //   for (int dir = 0; dir < 3; dir++)
     //   {
-    //     applyTwistedBoundaryConditions(field, dir);
-    //     return;
+    //     applyTwistedBoundaryConditions2(field, dir);
     //   }
+    //   return;
     // }
+    // if (boundaryConditions_[0] == -2 && boundaryConditions_[1] == 0 && boundaryConditions_[2] == 0)
+    // {
+    //   for (int dir = 0; dir < 3; dir++)
+    //   {
+    //     applyTwistedBoundaryConditions3(field, dir);
+    //   }
+    //   return;
+    // }   
     for (int dir = 0; dir < 3; dir++)
     {
-      if (boundaryConditions_[dir] == -1)
-      {
-        applyCPeriodicBoundaryConditions(field, dir);
-      }
+      applyCPeriodicBoundaryConditions(field, dir);
     }
   }
 
@@ -301,48 +307,45 @@ namespace monsta {
       {
         for (int matIdx = 0; matIdx < numFieldMatrices_; matIdx++) // Apply C-periodic boundary conditions
         {
+          int pauliMatNum = boundaryConditions_[dir];
           Matrix boundaryMat(field, site, matIdx);
           if (matIdx < 3)
           {
-            boundaryMat = pauli2*boundaryMat*pauli2;
-          }
-          field(site, matIdx, 0, 0) = boundaryMat(0, 0);
-          field(site, matIdx, 0, 1) = boundaryMat(0, 1);
-          field(site, matIdx, 1, 0) = boundaryMat(1, 0);
-          field(site, matIdx, 1, 1) = boundaryMat(1, 1);
-        }
-      }
-    }
-  }
-
-  void GeorgiGlashowSu2Theory::applyTwistedBoundaryConditions(LATfield2::Field< std::complex<double> > &field, int dir) const
-  {
-    LATfield2::Site site(field.lattice());
-
-    for (site.haloFirst(); site.haloTest(); site.haloNext())
-    {
-      int coord = site.coord(dir);
-      int maxCoord = field.lattice().size(dir) - 1;
-      if (coord < 0 || coord > maxCoord)
-      {
-        for (int matIdx = 0; matIdx < numFieldMatrices_; matIdx++) // Apply C-periodic boundary conditions
-        {
-          Matrix boundaryMat(field, site, matIdx);
-          if (matIdx < 3)
-          {
-            switch(dir)
+            switch(pauliMatNum)
             {
               case 0:
-                boundaryMat = pauli1*boundaryMat*pauli1;
                 break;
               case 1:
-                boundaryMat = pauli2*boundaryMat*pauli2;
+                boundaryMat = pauli1*boundaryMat*pauli1;
                 break;
               case 2:
+                boundaryMat = pauli2*boundaryMat*pauli2;
+                break;
+              case 3:
                 boundaryMat = pauli3*boundaryMat*pauli3;
                 break;
             }
+            if (tHooftLine_ && dir == 2 && site.coord(1) == 0)
+            {
+              if (matIdx == 1)
+              {
+                boundaryMat = -1.0*boundaryMat;
+              }
+            }
           }
+          else
+          {
+            if (pauliMatNum == 0) { break; }
+            if (pauliMatNum == 3)
+            {
+              boundaryMat(0,0) = -1.0*boundaryMat(0,0);
+            }
+            else
+            {
+              boundaryMat(0,0) = boundaryMat(0,0);
+            }
+          }
+
           field(site, matIdx, 0, 0) = boundaryMat(0, 0);
           field(site, matIdx, 0, 1) = boundaryMat(0, 1);
           field(site, matIdx, 1, 0) = boundaryMat(1, 0);
@@ -351,6 +354,70 @@ namespace monsta {
       }
     }
   }
+
+  // void GeorgiGlashowSu2Theory::applyTwistedBoundaryConditions2(LATfield2::Field< std::complex<double> > &field, int dir) const
+  // {
+  //   // cout << dir << endl;
+  //   LATfield2::Site site(field.lattice());
+
+  //   for (site.haloFirst(); site.haloTest(); site.haloNext())
+  //   {
+  //     int coord = site.coord(dir);
+  //     int maxCoord = field.lattice().size(dir) - 1;
+  //     if (coord < 0 || coord > maxCoord)
+  //     {
+  //       for (int matIdx = 0; matIdx < numFieldMatrices_; matIdx++) // Apply C-periodic boundary conditions
+  //       {
+  //         Matrix boundaryMat(field, site, matIdx);
+  //         if (dir == 0 && matIdx < 3)
+  //         {
+  //           boundaryMat = pauli2*boundaryMat*pauli2;
+  //         }
+  //         if (dir == 2 && site.coord(1) == 0)
+  //         {
+  //           if (matIdx == 1)
+  //           {
+  //             boundaryMat = -1.0*boundaryMat;
+  //           }
+  //         }
+  //         field(site, matIdx, 0, 0) = boundaryMat(0, 0);
+  //         field(site, matIdx, 0, 1) = boundaryMat(0, 1);
+  //         field(site, matIdx, 1, 0) = boundaryMat(1, 0);
+  //         field(site, matIdx, 1, 1) = boundaryMat(1, 1);
+  //       }
+  //     }
+  //   }
+  // }
+
+  // void GeorgiGlashowSu2Theory::applyTwistedBoundaryConditions3(LATfield2::Field< std::complex<double> > &field, int dir) const
+  // {
+  //   // cout << dir << endl;
+  //   LATfield2::Site site(field.lattice());
+
+  //   for (site.haloFirst(); site.haloTest(); site.haloNext())
+  //   {
+  //     int coord = site.coord(dir);
+  //     int maxCoord = field.lattice().size(dir) - 1;
+  //     if (coord < 0 || coord > maxCoord)
+  //     {
+  //       for (int matIdx = 0; matIdx < numFieldMatrices_; matIdx++) // Apply C-periodic boundary conditions
+  //       {
+  //         Matrix boundaryMat(field, site, matIdx);
+  //         if (dir == 2 && site.coord(1) == 0)
+  //         {
+  //           if (matIdx == 1)
+  //           {
+  //             boundaryMat = -1.0*boundaryMat;
+  //           }
+  //         }
+  //         field(site, matIdx, 0, 0) = boundaryMat(0, 0);
+  //         field(site, matIdx, 0, 1) = boundaryMat(0, 1);
+  //         field(site, matIdx, 1, 0) = boundaryMat(1, 0);
+  //         field(site, matIdx, 1, 1) = boundaryMat(1, 1);
+  //       }
+  //     }
+  //   }
+  // }
 
   monsta::Matrix GeorgiGlashowSu2Theory::getPlaquette(LATfield2::Field< std::complex<double> > &field, LATfield2::Site &site, int dir1, int dir2) const
   {
