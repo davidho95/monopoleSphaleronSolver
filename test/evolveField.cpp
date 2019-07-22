a5ac7550c7254f1a64e739445db5f66d5131bb10
David Ho <dlh16@dyn195-199.dyn.ic.ac.uk>
2019-07-22 09:44:52 +0100

    added routine to evolve existing field via standard gradient descent

---
+++ test/evolveField.cpp
@@ -0,0 +1,106 @@
+#include "LATfield2.hpp"
+#include <complex>
+#include "../src/GeorgiGlashowSu2TheoryUnitary.hpp"
+#include "../src/GeorgiGlashowSu2EomTheory.hpp"
+#include "../src/Matrix.hpp"
+#include "../src/GradDescentSolverBBStep.hpp"
+#include "../src/GradDescentSolverChigusa.hpp"
+#include "../src/GradDescentSolverCorePreserving.hpp"
+#include "../src/GradSqMinimiser.hpp"
+#include "../src/Su2Tools.hpp"
+#include "../src/MonopoleFileTools.hpp"
+#include "../src/MonopoleFieldTools.hpp"
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+
+int main(int argc, char **argv)
+{
+  clock_t begin = clock();
+
+  std::string outputPath;
+  std::string inputPath;
+  int sz = 16;
+  int n = 2;
+  int m = 2;
+  double vev = 1;
+  double gaugeCoupling = 1;
+  double selfCoupling = 1;
+  int sep = sz/2;
+  double xAspect = 1;
+
+  for (int i=1 ; i < argc ; i++ ){
+    if ( argv[i][0] != '-' )
+      continue;
+    switch(argv[i][1]) {
+      case 'p':
+        outputPath = argv[++i];
+        break;
+      case 's':
+        sz =  atoi(argv[++i]);
+        break;
+      case 'n':
+        n = atoi(argv[++i]);
+        break;
+      case 'm':
+        m = atoi(argv[++i]);
+        break;
+      case 'i':
+        inputPath = argv[++i];
+        break;
+      case 'v':
+        vev = atof(argv[++i]);
+        break;
+      case 'g':
+        gaugeCoupling = atof(argv[++i]);
+        break;
+      case 'l':
+        selfCoupling = atof(argv[++i]);
+        break;
+      case 'x':
+        xAspect = atof(argv[++i]);
+        break;
+    }
+  }
+
+  parallel.initialize(n,m);
+
+  int dim = 3;
+  int xSz = round(xAspect*sz);
+  int ySz = sz;
+  int zSz = sz;
+  int latSize[dim] = {xSz, ySz, zSz};
+  int haloSize = 2;
+  int numMatrices = 4;
+  int numRows = 2;
+  int numCols = 2;
+
+  LATfield2::Lattice lattice(dim, latSize, haloSize);
+  LATfield2::Field<complex<double> > field(lattice, numMatrices, numRows, numCols, 0);
+
+  LATfield2::Site site(lattice);
+
+  double initialStep = 0.01;
+  double maxStepSize = 0.05;
+  double tol = 1e-8;
+  int maxNumSteps = 20000;
+
+  monsta::GeorgiGlashowSu2Theory theory(gaugeCoupling, vev, selfCoupling, {0, 0, 0}, true);
+  monsta::readRawField(field, inputPath + "/rawData");
+  theory.applyBoundaryConditions(field);
+
+  monsta::GradDescentSolver solver(tol, maxNumSteps, initialStep, maxStepSize);
+
+  monsta::scaleVev(field, theory);
+
+  solver.solve(theory, field);
+
+  monsta::writeRawField(field, outputPath + "/rawData");
+  monsta::writeCoords(field, outputPath + "/coords");
+  monsta::writeHiggsFieldUnitary(field, outputPath + "/higgsData");
+  monsta::writeMagneticField(field, outputPath + "/magneticFieldData", theory);
+  monsta::writeEnergyDensity(field, outputPath + "/energyData", theory);
+  monsta::writeUnitaryGaugeField(field, outputPath + "/gaugeData");
+
+}
